ebefc210c65a0ffe4998806bbf51fee6b87b1f44
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2026-01-26 20:18:19 +0400

    feat(nix): improved project-wide nix architecture
    - Removal of flake-utils: https://ayats.org/blog/no-flake-utils
    - expose nix-systems on flake (formerly an input of flake-utils): https://github.com/nix-systems/nix-systems
    - introduced down stream overidability of package with `callPackage`: https://nixos.org/guides/nix-pills/13-callpackage-design-pattern
    - pruned redundancies in devShell and package
    - used stdenvAdapter for using mold linker in devShell
    - modularized flake, composing in a flake independent manner
      (allows easy non flake usage)
    - used filesets to prevent wasted rebuilds when non-source files change (readme, etc)

---
+++ nix/package.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  rustPlatform,
+  pkg-config,
+  clang,
+  makeBinaryWrapper,
+  mold,
+  dbus,
+  chafa,
+  glib,
+}:
+rustPlatform.buildRustPackage (final: let
+  inherit (lib.fileset) toSource unions;
+  inherit (lib) licenses platforms;
+in {
+  pname = "oxicord";
+  version = "0.1.7";
+
+  src = toSource {
+    root = ../.;
+    fileset = unions [
+      ../src
+      ../Cargo.lock
+      ../Cargo.toml
+    ];
+  };
+
+  cargoLock.lockFile = ../Cargo.lock;
+
+  nativeBuildInputs = [
+    pkg-config
+    clang
+    mold
+    makeBinaryWrapper
+  ];
+
+  buildInputs = [
+    dbus
+    chafa
+    glib
+  ];
+
+  meta = {
+    description = "A lightweight, secure Discord terminal client";
+    homepage = "https://github.com/linuxmobile/oxicord";
+    license = licenses.mit;
+    maintainers = [];
+    mainProgram = "oxicord";
+    platforms = platforms.unix;
+  };
+})
