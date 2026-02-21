9bb3d9779a10a575dac3cab9c6f74620b561ebb3
Braian A. Diez <bdiez19@gmail.com>
2026-02-21 19:49:11 -0300

    refactor(infrastructure):safe update_build_number in ClientIdentity
    Signed-off-by: Braian A. Diez <bdiez19@gmail.com>

--- src/infrastructure/discord/client.rs
+++ src/infrastructure/discord/client.rs
@@ -45,7 +45,9 @@ impl DiscordClient {
 
         tokio::spawn(async move {
             if let Some(build) = scraper::fetch_latest_build_number().await {
-                id_clone.update_build_number(build);
+                if let Err(e) = id_clone.update_build_number(build) {
+                    warn!("Failed to update build number: {}", e);
+                }
             }
         });
 
